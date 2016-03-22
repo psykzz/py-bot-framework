c83e957176dbb8ece7dde8fca93a2b7dcb3965fa
Matt Smith <matt.daemon660@gmail.com>
2016-03-22 09:33:36 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='SlackBotFramework',
+    version=1.0.0,
+    description="Slack bot framework",
+    long_description="Slack bot framework",
+    author='Matt Smith',
+    author_email='matt.daemon660@gmail.com',
+    url='https://github.com/psykzz/py-bot-framework',
+    packages=['SlackBotFramework'],
+    install_requires=[
+        "slackclient",
+    ]
+)
